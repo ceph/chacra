581c220f9e39751eeb0fdf56fcc25e5defe00ee4
Alfredo Deza <adeza@redhat.com>
2015-05-05 10:50:18 -0400

    fix controllers to return proper JSON
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/controllers/projects.py
+++ chacra/controllers/projects.py
@@ -1,38 +1,79 @@
 from pecan import expose, abort, request
-from chacra.models import projects
+from chacra.models import projects, Distro, DistroVersion, DistroArch, Binary
+
+
+class BinaryController(object):
+
+    def __init__(self, binary_name):
+        self.binary_name = binary_name
+        self.binary = Binary.query.filter_by(name=binary_name).first()
+        if not self.binary:
+            abort(404)
+
+    @expose('json')
+    def index(self):
+        # TODO: implement downloads
+        return dict(name=self.binary.name)
+
+
+class ArchController(object):
+
+    def __init__(self, arch_name):
+        self.arch_name = arch_name
+        self.distro_arch = DistroArch.query.filter_by(name=arch_name).first()
+        if not self.distro_arch:
+            abort(404)
+        request.context['distro_arch_id'] = self.distro_arch.id
+
+    @expose('json')
+    def index(self):
+        return dict(
+            (d.name, d) for d in self.distro_arch.binaries.all()
+        )
+
+
+    @expose()
+    def _lookup(self, name, *remainder):
+        return BinaryController(name), remainder
+
+
+class DistroVersionController(object):
+
+    def __init__(self, version_name):
+        self.version_name = version_name
+        self.distro_version = DistroVersion.query.filter_by(name=version_name).first()
+        if not self.distro_version:
+            abort(404)
+        request.context['distro_version_id'] = self.distro_version.id
+
+    @expose('json')
+    def index(self):
+        return dict(
+            (d.name, d) for d in self.distro_version.archs.all()
+        )
+
+    @expose()
+    def _lookup(self, name, *remainder):
+        return ArchController(name), remainder
 
 
 class DistroController(object):
-
-    def __init__(self, name):
-        self.name = name
-        project_id = request.context['project_id']
-        self.project = projects.Project.get(project_id)
-
-    @expose('json')
-    def index(self):
-        distro = self.project.get_distro(self.name)
-        if not distro:
+    def __init__(self, distro_name):
+        self.distro_name = distro_name
+        self.distro = Distro.query.filter_by(name=distro_name).first()
+        if not self.distro:
             abort(404)
-        return distro
-
-
-class DistrosController(object):
-
-    def __init__(self, name):
-        self.name = name
-        project_id = request.context['project_id']
-        self.project = projects.Project.get(project_id)
+        request.context['distro_id'] = self.distro.id
 
     @expose('json')
     def index(self):
-        _id = request.context['project_id']
-        project = projects.Project.get(_id)
-        return project.distros
+        return dict(
+            (d.name, d) for d in self.distro.versions.all()
+        )
 
-    @expose('json')
+    @expose()
     def _lookup(self, name, *remainder):
-        return DistroController(name), remainder
+        return DistroVersionController(name), remainder
 
 
 class ProjectController(object):
@@ -46,18 +87,23 @@ class ProjectController(object):
 
     @expose('json')
     def index(self):
-        return self.project.distros
+        return dict(
+            (d.name, d) for d in self.project.distros.all()
+        )
 
-    @expose
+    @expose()
     def _lookup(self, name, *remainder):
-        return DistrosController(name), remainder
+        return DistroController(name), remainder
 
 
 class ProjectsController(object):
 
     @expose('json')
     def index(self):
-        return projects.Project.query.all()
+        return dict(
+            (p.name, p) for p in
+            projects.Project.query.all()
+        )
 
     @expose()
     def _lookup(self, project_name, *remainder):
---
+++ chacra/controllers/util.py
@@ -0,0 +1,83 @@
+from datetime import datetime, timedelta
+
+
+def last_seen(timestamp):
+    now = datetime.utcnow()
+    difference = now - timestamp
+    formatted = ReadableSeconds(difference.seconds)
+    return "%s ago" % formatted
+
+
+class ReadableSeconds(object):
+
+    def __init__(self, seconds):
+        self.original_seconds = seconds
+
+    @property
+    def relative(self):
+        """
+        Generate a relative datetime object based on current seconds
+        """
+        return datetime(1, 1, 1) + timedelta(seconds=self.original_seconds)
+
+    def __str__(self):
+        return "{0}{1}{2}{3}{4}{5}".format(
+            self.years,
+            self.months,
+            self.days,
+            self.hours,
+            self.minutes,
+            self.seconds,
+        ).rstrip(' ,')
+
+    @property
+    def years(self):
+        # Subtract 1 here because the earliest datetime() is 1/1/1
+        years = self.relative.year - 1
+        year_str = 'years' if years > 1 else 'year'
+        if years:
+            return "%d %s, " % (years, year_str)
+        return ""
+
+    @property
+    def months(self):
+        # Subtract 1 here because the earliest datetime() is 1/1/1
+        months = self.relative.month - 1
+        month_str = 'months' if months > 1 else 'month'
+        if months:
+            return "%d %s, " % (months, month_str)
+        return ""
+
+    @property
+    def days(self):
+        # Subtract 1 here because the earliest datetime() is 1/1/1
+        days = self.relative.day - 1
+        day_str = 'days' if days > 1 else 'day'
+        if days:
+            return "%d %s, " % (days, day_str)
+        return ""
+
+    @property
+    def hours(self):
+        hours = self.relative.hour
+        hour_str = 'hours' if hours > 1 else 'hour'
+        if hours:
+            return "%d %s, " % (self.relative.hour, hour_str)
+        return ""
+
+    @property
+    def minutes(self):
+        minutes = self.relative.minute
+        minutes_str = 'minutes' if minutes > 1 else 'minute'
+        if minutes:
+            return "%d %s, " % (self.relative.minute, minutes_str)
+        return ""
+
+    @property
+    def seconds(self):
+        seconds = self.relative.second
+        seconds_str = 'seconds' if seconds > 1 else 'second'
+        if seconds:
+            return "%d %s, " % (self.relative.second, seconds_str)
+        return ""
+
