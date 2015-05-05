a7c77dca49db4f08409e1afd45490b95c069d430
Alfredo Deza <adeza@redhat.com>
2015-05-05 05:02:56 -0400

    some basic stuff for testing
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ chacra/tests/config.py
@@ -0,0 +1,71 @@
+from pecan.hooks import TransactionHook, RequestViewerHook
+from chacra import models
+
+
+# Server Specific Configurations
+server = {
+    'port': '8080',
+    'host': '0.0.0.0'
+}
+
+# Pecan Application Configurations
+app = {
+    'root': 'chacra.controllers.root.RootController',
+    'modules': ['chacra'],
+    'static_root': '%(confdir)s/public',
+    'default_renderer': 'json',
+    'hooks': [
+        TransactionHook(
+            models.start,
+            models.start_read_only,
+            models.commit,
+            models.rollback,
+            models.clear
+        ),
+    ],
+    'debug': True,
+    #'errors': {
+    #    404: '/error/404',
+    #    '__force_dict__': True
+    #}
+}
+
+logging = {
+    'loggers': {
+        'root': {'level': 'INFO', 'handlers': ['console']},
+        'chacra': {'level': 'DEBUG', 'handlers': ['console']},
+        'pecan.commands.serve': {'level': 'DEBUG', 'handlers': ['console']},
+        'py.warnings': {'handlers': ['console']},
+        '__force_dict__': True
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'color'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        },
+        'color': {
+            '()': 'pecan.log.ColorFormatter',
+            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
+                       '[%(threadName)s] %(message)s'),
+        '__force_dict__': True
+        }
+    }
+}
+
+sqlalchemy = {
+    # You may use SQLite for testing
+    'url': 'sqlite:///dev.db',
+    # When you set up PostreSQL, it will look more like:
+    #'url': 'postgresql+psycopg2://USER:PASSWORD@DB_HOST/DB_NAME',
+    'echo':          True,
+    'echo_pool':     True,
+    'pool_recycle':  3600,
+    'encoding':      'utf-8'
+}
---
+++ chacra/tests/conftest.py
@@ -0,0 +1,170 @@
+import os
+from pecan.testing import load_test_app
+
+import subprocess
+
+from copy import deepcopy
+from pecan import conf
+from pecan import configuration
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+
+from chacra import models as _db
+import pytest
+
+
+DBNAME = 'chacratest'
+BIND = 'postgresql+psycopg2://localhost'
+
+
+def config_file():
+    here = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(here, 'config.py')
+
+
+@pytest.fixture(scope='session')
+def app(request):
+    config = configuration.conf_from_file(config_file()).to_dict()
+
+    # Add the appropriate connection string to the app config.
+    config['sqlalchemy'] = {
+        'url': '%s/%s' % (BIND, DBNAME),
+        'encoding': 'utf-8',
+        'poolclass': NullPool
+    }
+
+    # Set up a fake app
+    app = TestApp(load_test_app(config))
+    return app
+
+
+@pytest.fixture(scope='session')
+def connection(app, request):
+    """Session-wide test database."""
+    # Connect and create the temporary database
+    print "=" * 80
+    print "CREATING TEMPORARY DATABASE FOR TESTS"
+    print "=" * 80
+    subprocess.call(['dropdb', DBNAME])
+    subprocess.call(['createdb', DBNAME])
+
+    # Bind and create the database tables
+    _db.clear()
+    engine_url = '%s/%s' % (BIND, DBNAME)
+
+    db_engine = create_engine(
+        engine_url,
+        encoding='utf-8',
+        poolclass=NullPool)
+
+    # AKA models.start()
+    _db.Session.bind = db_engine
+    _db.metadata.bind = _db.Session.bind
+
+    _db.Base.metadata.create_all(db_engine)
+    _db.commit()
+    _db.clear()
+
+    #connection = db_engine.connect()
+
+    def teardown():
+        _db.Base.metadata.drop_all(db_engine)
+
+    request.addfinalizer(teardown)
+
+    # Slap our test app on it
+    _db.app = app
+    return _db
+
+
+@pytest.fixture(scope='function')
+def session(connection, request):
+    """Creates a new database session for a test."""
+    _config = configuration.conf_from_file(config_file()).to_dict()
+    config = deepcopy(_config)
+
+    # Add the appropriate connection string to the app config.
+    config['sqlalchemy'] = {
+        'url': '%s/%s' % (BIND, DBNAME),
+        'encoding': 'utf-8',
+        'poolclass': NullPool
+    }
+
+    connection.start()
+
+    def teardown():
+        from sqlalchemy.engine import reflection
+
+        # Tear down and dispose the DB binding
+        connection.clear()
+
+        # start a transaction
+        engine = conf.sqlalchemy.engine
+        conn = engine.connect()
+        trans = conn.begin()
+
+        inspector = reflection.Inspector.from_engine(engine)
+
+        # gather all data first before dropping anything.
+        # some DBs lock after things have been dropped in
+        # a transaction.
+        conn.execute("TRUNCATE TABLE %s RESTART IDENTITY CASCADE" % (
+            ', '.join(inspector.get_table_names())
+        ))
+
+        trans.commit()
+        conn.close()
+
+    request.addfinalizer(teardown)
+    return connection
+
+
+class TestApp(object):
+    """
+    A controller test starts a database transaction and creates a fake
+    WSGI app.
+    """
+
+    __headers__ = {}
+
+    def __init__(self, app):
+        self.app = app
+
+    def _do_request(self, url, method='GET', **kwargs):
+        methods = {
+            'GET': self.app.get,
+            'POST': self.app.post,
+            'PUT': self.app.put,
+            'DELETE': self.app.delete
+        }
+        kwargs.setdefault('headers', {}).update(self.__headers__)
+        return methods.get(method, self.app.get)(str(url), **kwargs)
+
+    def post(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a POST request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'POST', **kwargs)
+
+    def get(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a GET request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'GET', **kwargs)
+
+    def put(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a PUT request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'PUT', **kwargs)
+
+    def delete(self, url, **kwargs):
+        """
+        @param (string) url - The URL to emulate a DELETE request to
+        @returns (paste.fixture.TestResponse)
+        """
+        return self._do_request(url, 'DELETE', **kwargs)
+
