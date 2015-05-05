e6206aafe000293bb14ce9eaacf7e5212c93f7f0
Alfredo Deza <adeza@redhat.com>
2015-05-05 08:18:42 -0400

    fix all broken tests for project controllers
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/tests/controllers/test_projects.py
+++ chacra/tests/controllers/test_projects.py
@@ -1,54 +1,60 @@
-from chacra.models import Package
+from chacra.models import Project
 
 
-class TestPackagesController(object):
+class TestProjectsController(object):
 
-    def test_get_index_no_packages(self, session):
-        result = session.app.get('/packages/')
+    def test_get_index_no_projects(self, session):
+        result = session.app.get('/projects/')
         assert result.status_int == 200
         assert result.json == {}
 
     def test_list_a_project(self, session):
-        Package('foobar', 'example.com')
+        Project('foobar')
         session.commit()
+        result = session.app.get('/projects/')
+        assert result.json['foobar']['name'] == 'foobar'
 
-        result = session.app.get('/packages/')
-        json = result.json['packages']
+    def test_single_project_should_have_one_item(self, session):
+        Project('foobar')
+        session.commit()
+        result = session.app.get('/projects/')
         assert result.status_int == 200
-        assert len(json) == 1
+        assert len(result.json) == 1
 
-    def test_list_a_couple_of_packages(self, session):
+    def test_list_a_few_projects(self, session):
         for p in range(20):
-            Package('foo_%s' % p, 'example.com')
+            Project('foo_%s' % p)
         session.commit()
 
-        result = session.app.get('/packages/')
-        json = result.json['packages']
+        result = session.app.get('/projects/')
+        json = result.json
         assert result.status_int == 200
         assert len(json) == 20
 
-    def test_get_extra_metadata_for_package(self, session):
-        Package('foobar', 'example.com')
-        session.commit()
+# TODO
+#    def test_get_extra_metadata_for_package(self, session):
+#        Project('foobar')
+#        session.commit()
+#
+#        result = session.app.get('/projects/')
+#        last_updated = result.json['foobar']['last_updated']
+#        assert last_updated.endswith('seconds ago')
 
-        result = session.app.get('/packages/')
-        json = result.json['packages']
-        last_updated = json['foobar']['last_updated']
-        assert last_updated.endswith('seconds ago')
 
-
-class TestPackageController(object):
+class TestProjectController(object):
 
     def test_get_index_single_project(self, session):
-        Package('foobar', 'example.com')
-        result = session.app.get('/packages/foobar/')
+        Project('foobar')
+        session.commit()
+        result = session.app.get('/projects/foobar/')
         assert result.status_int == 200
 
     def test_get_index_no_project(self, session):
-        result = session.app.get('/packages/foobar/', expect_errors=True)
+        result = session.app.get('/projects/foobar/', expect_errors=True)
         assert result.status_int == 404
 
     def test_get_index_single_project_data(self, session):
-        Package('foobar', 'example.com')
-        result = session.app.get('/packages/foobar/')
+        Project('foobar')
+        session.commit()
+        result = session.app.get('/projects/foobar/')
         assert result.json == {}
