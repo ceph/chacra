69ccffbe469de6164d6a623a3c4eb31a5a97ef15
Alfredo Deza <adeza@redhat.com>
2015-05-05 13:28:54 -0400

    add helpers to ensure objects are present
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/controllers/projects.py
+++ chacra/controllers/projects.py
@@ -1,5 +1,20 @@
 from pecan import expose, abort, request
-from chacra.models import projects, Distro, DistroVersion, DistroArch, Binary
+from chacra.models import projects, Distro, DistroVersion, DistroArch, Binary, commit
+from chacra import models
+from chacra.controllers import error
+
+
+# TODO: move this out of here
+def set_id_in_context(name, object_model, value):
+    # if the object_model is None, then it will save it as None
+    # saving us from having to do this everywhere
+    object_name = name.split('_id')[0]
+    if object_model is not None:
+        request.context[name] = object_model.id
+        request.context[object_name] = object_model.name
+    else:
+        request.context[name] = None
+        request.context[object_name] = value
 
 
 class BinaryController(object):
@@ -7,13 +22,49 @@ class BinaryController(object):
     def __init__(self, binary_name):
         self.binary_name = binary_name
         self.binary = Binary.query.filter_by(name=binary_name).first()
-        if not self.binary:
-            abort(404)
+        if not self.binary and request.method != 'POST':
+                abort(404)
 
-    @expose('json')
+    @expose('json', generic=True)
     def index(self):
         # TODO: implement downloads
         return dict(name=self.binary.name)
+
+    @index.when(method='POST', template='json')
+    def index_post(self):
+        # updates the binary
+        return
+        try:
+            data = request.json
+            name = data.get('name')
+        except ValueError:
+            error('/errors/invalid/', 'could not decode JSON body')
+        # we allow empty data to be pushed
+        if not name:
+            error('/errors/invalid/', "could not find required key: 'name'")
+        self.ensure_objects(name)
+        return True
+
+    def ensure_objects(self, binary_name):
+        """
+        Since we might not have everything created, ensure everything is
+        and push it to the database
+        """
+        project_id = request.context.get('project_id')
+        distro_id = request.context.get('distro_id')
+        version_id = request.context.get('version_id')
+        arch_id = request.context.get('distro_arch_id')
+        is_none = lambda x: x is None
+        if all(
+                [is_none(i) for i in [project_id, distro_id, version_id, arch_id]]):
+            project = projects.Project(request.context['project'])
+            distro = Distro(request.context['distro'], project)
+            version = DistroVersion(request.context['version'], distro)
+            arch = DistroArch(request.context['distro_arch'], version)
+            models.flush()
+            models.commit()
+            binary = Binary(binary_name, arch)
+            return binary
 
 
 class ArchController(object):
@@ -21,16 +72,51 @@ class ArchController(object):
     def __init__(self, arch_name):
         self.arch_name = arch_name
         self.distro_arch = DistroArch.query.filter_by(name=arch_name).first()
-        if not self.distro_arch:
+        if not self.distro_arch and request.method != 'POST':
             abort(404)
-        request.context['distro_arch_id'] = self.distro_arch.id
+        set_id_in_context('distro_arch_id', self.distro_arch, arch_name)
 
-    @expose('json')
+    @expose(generic=True, template='json')
     def index(self):
         return dict(
             (d.name, d) for d in self.distro_arch.binaries.all()
         )
 
+    @index.when(method='POST', template='json')
+    def index_post(self):
+        # updates the binary
+        try:
+            data = request.json
+            name = data.get('name')
+        except ValueError:
+            error('/errors/invalid/', 'could not decode JSON body')
+        # we allow empty data to be pushed
+        if not name:
+            error('/errors/invalid/', "could not find required key: 'name'")
+        self.ensure_objects(name)
+        return True
+
+    def ensure_objects(self, binary_name):
+        """
+        Since we might not have everything created, ensure everything is
+        and push it to the database
+        """
+        project_id = request.context.get('project_id')
+        distro_id = request.context.get('distro_id')
+        version_id = request.context.get('distro_version_id')
+        arch_id = request.context.get('distro_arch_id')
+        is_none = lambda x: x is None
+        print request.context
+        if all(
+                [is_none(i) for i in [project_id, distro_id, version_id, arch_id]]):
+            project = projects.Project(request.context['project'])
+            distro = Distro(request.context['distro'], project)
+            version = DistroVersion(request.context['distro_version'], distro)
+            arch = DistroArch(request.context['distro_arch'], version)
+            models.flush()
+            models.commit()
+            binary = Binary(binary_name, arch)
+            return binary
 
     @expose()
     def _lookup(self, name, *remainder):
@@ -42,9 +128,9 @@ class DistroVersionController(object):
     def __init__(self, version_name):
         self.version_name = version_name
         self.distro_version = DistroVersion.query.filter_by(name=version_name).first()
-        if not self.distro_version:
+        if not self.distro_version and request.method != 'POST':
             abort(404)
-        request.context['distro_version_id'] = self.distro_version.id
+        set_id_in_context('distro_version_id', self.distro_version, version_name)
 
     @expose('json')
     def index(self):
@@ -61,9 +147,9 @@ class DistroController(object):
     def __init__(self, distro_name):
         self.distro_name = distro_name
         self.distro = Distro.query.filter_by(name=distro_name).first()
-        if not self.distro:
+        if not self.distro and request.method != 'POST':
             abort(404)
-        request.context['distro_id'] = self.distro.id
+        set_id_in_context('distro_id', self.distro, distro_name)
 
     @expose('json')
     def index(self):
@@ -81,9 +167,9 @@ class ProjectController(object):
     def __init__(self, project_name):
         self.project_name = project_name
         self.project = projects.Project.query.filter_by(name=project_name).first()
-        if not self.project:
+        if not self.project and request.method != 'POST':
             abort(404)
-        request.context['project_id'] = self.project.id
+        set_id_in_context('project_id', self.project, project_name)
 
     @expose('json')
     def index(self):
