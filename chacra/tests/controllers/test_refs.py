1d6eb56f5ca45b69e14f4be8db2d61ab7f788e62
Alfredo Deza <adeza@redhat.com>
2015-06-11 10:05:14 -0400

    add test for refs with distros
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/tests/controllers/test_refs.py
+++ chacra/tests/controllers/test_refs.py
@@ -8,7 +8,7 @@ class TestRefController(object):
         Ref('master', p)
         session.commit()
         result = session.app.get('/projects/ceph/master/')
-        assert result.status_int == 200
+        assert result.json == {}
 
     def test_get_index_no_ref(self, session):
         Project('ceph')
@@ -16,11 +16,19 @@ class TestRefController(object):
         result = session.app.get('/projects/ceph/next/', expect_errors=True)
         assert result.status_int == 404
 
-    def test_get_index_single_ref_data(self, session):
+    def test_get_index_ref_with_distro(self, session):
+        p = Project('ceph')
+        r = Ref('master', p)
+        Distro('centos', r)
+        session.commit()
+        result = session.app.get('/projects/ceph/master/')
+        assert result.json['centos'] == {'name': 'centos', 'versions': []}
+
+    def test_get_index_ref_with_distros(self, session):
         p = Project('ceph')
         r = Ref('master', p)
         Distro('ubuntu', r)
+        Distro('centos', r)
         session.commit()
         result = session.app.get('/projects/ceph/master/')
-        assert result.json == {}
-
+        assert result.json.keys() == ['centos', 'ubuntu']
