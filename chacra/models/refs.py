dfe565bd682b53628311fb50a76cd12b386e460a
Alfredo Deza <adeza@redhat.com>
2015-06-15 11:14:34 -0400

    refs have distros, list them in JSON
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/models/refs.py
+++ chacra/models/refs.py
@@ -19,6 +19,6 @@ class Ref(Base):
     def __json__(self):
         return dict(
             name=self.name,
-            distros=self.distros.all()
+            distros=[d.name for d in self.distros.all()]
         )
 
