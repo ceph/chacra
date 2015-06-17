850643b5889c2cf371c5662e936a400683f5d070
Alfredo Deza <adeza@redhat.com>
2015-06-17 11:01:10 -0400

    move the set_id_in_context helper to init in controllers
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/controllers/__init__.py
+++ chacra/controllers/__init__.py
@@ -7,3 +7,15 @@ def error(url, msg=None):
         request.context['error_message'] = msg
     url = path.join(url, '?error_message=%s' % msg)
     redirect(url, internal=True)
+
+
+def set_id_in_context(name, object_model, value):
+    # if the object_model is None, then it will save it as None
+    # saving us from having to do this everywhere
+    object_name = name.split('_id')[0]
+    if object_model is not None:
+        request.context[name] = object_model.id
+        request.context[object_name] = object_model.name
+    else:
+        request.context[name] = None
+        request.context[object_name] = value
