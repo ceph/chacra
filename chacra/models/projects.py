55a19dd3dbdd3e558c1e6d3666ad0bbb325c21ad
Alfredo Deza <adeza@redhat.com>
2015-06-18 09:27:41 -0400

    port projects to use binaries as its only child
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/models/projects.py
+++ chacra/models/projects.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm.exc import DetachedInstanceError
 from chacra.models import Base
 
 
@@ -11,6 +11,12 @@ class Project(Base):
 
     def __init__(self, name):
         self.name = name
+
+    def __repr__(self):
+        try:
+            return '<Project %r>' % self.name
+        except DetachedInstanceError:
+            return '<Project detached>'
 
     def __json__(self):
         return dict(
