0a56dd86ed3d6ea1ab55af7f26271fcd5869d5dd
Alfredo Deza <adeza@redhat.com>
2015-06-23 19:38:18 -0400

    update tests for archs and binaries - passing
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/tests/controllers/test_archs.py
+++ chacra/tests/controllers/test_archs.py
@@ -4,42 +4,36 @@ from chacra.models import Project, Binary
 class TestArchController(object):
 
     def test_list_arch_no_binaries(self, session):
+        Project('ceph')
+        session.commit()
+        result = session.app.get('/projects/ceph/giant/centos/el6/x86_64/', expect_errors=True)
+        assert result.status_int == 404
+
+    def test_list_arch_one_binary(self, session):
         project = Project('ceph')
         Binary('ceph-1.0.0.rpm', project, ref='giant', distro='centos', distro_version='el6', arch='x86_64')
         session.commit()
         result = session.app.get('/projects/ceph/giant/centos/el6/x86_64/')
-        assert result.json == {}
+        assert result.json['ceph-1.0.0.rpm']
 
     def test_single_arch_should_have_one_item(self, session):
-        project = Project('ceph')
-        ref = Ref('giant', project)
-        distro = Distro('centos', ref, project)
-        version = DistroVersion('el6', distro, project)
-        arch = DistroArch('x86_64', version, project)
-        Binary('ceph-9.0.0-0.el6.x86_64.rpm', arch, project)
+        p = Project('ceph')
+        Binary('ceph-9.0.0-0.el6.x86_64.rpm', p, ref='giant', distro='centos', distro_version='el6', arch='x86_64')
         session.commit()
         result = session.app.get('/projects/ceph/giant/centos/el6/x86_64/')
         assert result.status_int == 200
         assert len(result.json) == 1
 
     def test_single_binary_should_not_be_signed(self, session):
-        project = Project('ceph')
-        ref = Ref('giant', project)
-        distro = Distro('centos', ref, project)
-        version = DistroVersion('el6', distro, project)
-        arch = DistroArch('x86_64', version, project)
-        Binary('ceph-9.0.0-0.el6.x86_64.rpm', arch, project)
+        p = Project('ceph')
+        Binary('ceph-9.0.0-0.el6.x86_64.rpm', p, ref='giant', distro='centos', distro_version='el6', arch='x86_64')
         session.commit()
         result = session.app.get('/projects/ceph/giant/centos/el6/x86_64/')
         assert result.json['ceph-9.0.0-0.el6.x86_64.rpm']['signed'] is False
 
     def test_single_binary_should_have_default_size_cero(self, session):
-        project = Project('ceph')
-        ref = Ref('giant', project)
-        distro = Distro('centos', ref, project)
-        version = DistroVersion('el6', distro, project)
-        arch = DistroArch('x86_64', version, project)
-        Binary('ceph-9.0.0-0.el6.x86_64.rpm', arch, project)
+        p = Project('ceph')
+        Binary('ceph-9.0.0-0.el6.x86_64.rpm', p, ref='giant', distro='centos', distro_version='el6', arch='x86_64')
         session.commit()
         result = session.app.get('/projects/ceph/giant/centos/el6/x86_64/')
         assert result.json['ceph-9.0.0-0.el6.x86_64.rpm']['size'] == 0
@@ -75,7 +69,6 @@ class TestArchController(object):
             params=dict(name='ceph-9.1.0-0.el6.x86_64.rpm'))
         el6 = session.app.get('/projects/ceph/giant/ceph/el6/x86_64/')
         el7 = session.app.get('/projects/ceph/giant/ceph/el7/x86_64/')
-        #assert el6.json.keys() == ['ceph-9.1.0-0.el6.x86_64.rpm']
         assert len(el6.json.keys()) == 1
         assert len(el7.json.keys()) == 1
 
---
+++ chacra/tests/controllers/test_binaries.py
@@ -0,0 +1,109 @@
+import os
+import pecan
+from chacra.models import Binary
+
+
+class TestBinaryUniqueness(object):
+    # ensure that there is no pollution from other projects or architectures
+
+    def test_two_projects_different_archs(self, session, tmpdir):
+        pecan.conf.binary_root = str(tmpdir)
+        session.app.post(
+            '/projects/ceph/giant/centos/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+
+        session.app.post(
+            '/projects/ceph-deploy/master/centos/el6/i386/ceph-deploy-1.0.0-0.el6.i386.rpm/',
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+
+        # get archs for ceph-deploy
+        result = session.app.get(
+            '/projects/ceph-deploy/master/centos/el6/',
+        )
+
+        assert result.json['el6'] == ['i386']
+
+
+class TestBinaryController(object):
+
+    def test_single_binary_file_creates_resource(self, session, tmpdir):
+        pecan.conf.binary_root = str(tmpdir)
+        result = session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            params={'force': 1},
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        assert result.status_int == 201
+
+    def test_new_binary_upload_creates_model_with_path_forced(self, session, tmpdir):
+        pecan.conf.binary_root = str(tmpdir)
+
+        session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            params={'force': '1'},
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            params={'force': '1'},
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            params={'force': '1'},
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+
+        binary = Binary.get(1)
+        assert binary.path.endswith('ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm')
+
+    def test_new_binary_upload_fails_with_existing(self, session, tmpdir):
+        pecan.conf.binary_root = str(tmpdir)
+
+        # we do a bunch of requests that do talk to the database
+        session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        result = session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')],
+            expect_errors=True
+        )
+
+        assert result.status_int == 400
+
+    def test_posting_twice_requires_force_flag(self, session, tmpdir):
+        pecan.conf.binary_root = str(tmpdir)
+        result = session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        result = session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            params={'force': True},
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        assert result.status_int == 200
+
+    def test_posting_twice_updates_the_binary(self, session, tmpdir):
+        pecan.conf.binary_root = str(tmpdir)
+        session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'hello tharrrr')]
+        )
+        session.app.post(
+            '/projects/ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm/',
+            params={'force': True},
+            upload_files=[('file', 'ceph-9.0.0-0.el6.x86_64.rpm', 'something changed')]
+        )
+
+        destination = os.path.join(
+            pecan.conf.binary_root,
+            'ceph/giant/ceph/el6/x86_64/ceph-9.0.0-0.el6.x86_64.rpm'
+        )
+
+        contents = open(destination).read()
+        assert contents == 'something changed'
