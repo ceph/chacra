3a9f9ed13d7c7dcbeb784b2ffc8d191b2281a90f
Alfredo Deza <adeza@redhat.com>
2015-06-29 11:44:47 -0400

    create a search controller
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ chacra/controllers/search.py
@@ -0,0 +1,35 @@
+from pecan import expose
+from chacra.models import Binary
+
+
+class SearchController(object):
+
+    def __init__(self):
+        self.filters = {
+                'distro': Binary.distro,
+                'distro_version': Binary.distro_version,
+                'arch': Binary.arch,
+                'ref': Binary.ref,
+                'built_by': Binary.built_by,
+                'size': Binary.size,
+                'name': Binary.name,
+                'name-like': Binary.name.like,
+        }
+
+    @expose('json')
+    def index(self, **kw):
+        query = self.apply_filters(kw)
+        return query.all()
+
+    def apply_filters(self, filters):
+        # TODO: allow operators
+        query = None
+        for k, v in filters.items():
+            if k in self.filters:
+                query = self.filter_binary(self.filters[k], v, query)
+        return query
+
+    def filter_binary(self, key, value, query=None):
+        if query:
+            return query.filter(key == value)
+        return Binary.query.filter(key == value)
