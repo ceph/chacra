e030ea7ca9ab5e53781effd5fe81de04166c4ca3
Alfredo Deza <adeza@redhat.com>
2015-07-14 14:25:43 -0400

    set the nginx redirect headers if the option is enabled
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/controllers/binaries.py
+++ chacra/controllers/binaries.py
@@ -46,10 +46,11 @@ class BinaryController(object):
         # TODO: maybe disable this for testing?
         # XXX Maybe we don't need to set Content-Disposition here?
         response.headers['Content-Disposition'] = 'attachment; filename=%s' % str(self.binary.name)
-        response.headers['X-Accel-Redirect'] = str(self.binary.path)
         if conf.delegate_downloads is False:
             f = open(self.binary.path, 'rb')
             response.app_iter = FileIter(f)
+        else:
+            response.headers['X-Accel-Redirect'] = str(self.binary.path)
 
     @index.when(method='POST', template='json')
     def index_post(self):
