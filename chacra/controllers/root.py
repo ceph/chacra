43b7ea4181a89c6dc3d06d9b5a6a59a5a4dc85ea
Alfredo Deza <adeza@redhat.com>
2015-07-14 12:50:33 -0400

    return a bit of info for root
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/controllers/root.py
+++ chacra/controllers/root.py
@@ -2,13 +2,23 @@ from pecan import expose
 from chacra.controllers.projects import ProjectsController
 from chacra.controllers.errors import ErrorsController
 from chacra.controllers.search import SearchController
+from chacra.models import Project
+
+
+description = """chacra is a binary API that allows querying, posting,
+updating, and retrieving binaries for different projects."""
 
 
 class RootController(object):
 
     @expose('json')
     def index(self):
-        return dict()
+        documentation = "https://github.com/alfredodeza/chacra#chacra"
+        projects = [p.name for p in Project.query.all()]
+        return dict(
+            description=description,
+            documentation=documentation,
+            projects=projects)
 
     projects = ProjectsController()
     errors = ErrorsController()
