fd91654e5afe7ccdb6bcdcf28a77ff1cae9f8ac8
Alfredo Deza <adeza@redhat.com>
2015-07-15 11:20:21 -0400

    filter by ref as well when returning what is available for distro_versions
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/controllers/distros.py
+++ chacra/controllers/distros.py
@@ -44,15 +44,20 @@ class DistroController(object):
     def __init__(self, distro_name):
         self.distro_name = distro_name
         self.project = models.Project.get(request.context['project_id'])
+        self.ref = request.context['ref']
         request.context['distro'] = distro_name
 
     @expose('json', generic=True)
     def index(self):
+        # TODO: Improve this duplication here (and spread to other controllers)
         if self.distro_name not in self.project.distros:
             abort(404)
+        if self.ref not in self.project.refs:
+            abort(404)
         resp = {}
+
         for version in self.project.distro_versions:
-            archs = [b.arch for b in models.Binary.filter_by(project=self.project, distro_version=version, distro=self.distro_name).all()]
+            archs = [b.arch for b in models.Binary.filter_by(project=self.project, distro_version=version, distro=self.distro_name, ref=self.ref).all()]
             if archs:
                 resp[version] = list(set(archs))
         return resp
