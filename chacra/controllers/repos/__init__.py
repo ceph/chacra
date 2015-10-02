ea8457cea7148d661bc7419f9dfbc63582b55081
Andrew Schoen <aschoen@redhat.com>
2015-10-02 11:43:50 -0500

    Remove RepoApiController and make it a controller method on RepoController
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- chacra/controllers/repos/__init__.py
+++ chacra/controllers/repos/__init__.py
@@ -58,27 +58,8 @@ class RepoController(object):
         error('/errors/not_allowed',
               'POST requests to this url are not allowed')
 
-    @expose()
-    def _lookup(self, name, *remainder):
-        return RepoApiController(name), remainder
-
-
-class RepoApiController(object):
-
-    def __init__(self, api):
-        self.distro_version = request.context['distro_version']
-        self.project = Project.get(request.context['project_id'])
-        self.distro_name = request.context['distro']
-        self.ref = request.context['ref']
-        request.context['distro_version'] = self.distro_version
-        self.repo = self.project.built_repos.filter_by(
-            distro=self.distro_name,
-            distro_version=self.distro_version,
-            ref=self.ref,
-        ).first()
-
     @expose('json', generic=True)
-    def index(self):
+    def api(self):
         if self.distro_version not in self.project.repo_distro_versions:
             abort(404)
 
@@ -91,12 +72,12 @@ class RepoApiController(object):
         return self.repo
 
     #TODO: post/delete will be supported eventually
-    @index.when(method='POST', template='json')
-    def index_post(self):
+    @api.when(method='POST', template='json')
+    def api_post(self):
         error('/errors/not_allowed',
               'POST requests to this url are not allowed')
 
-    @index.when(method='DELETE', template='json')
-    def index_delete(self):
+    @api.when(method='DELETE', template='json')
+    def api_delete(self):
         error('/errors/not_allowed',
               'DELETE requests to this url are not allowed')
