6a9c0b884fba59f102561a4d360d3f7ab117e552
Alfredo Deza <adeza@redhat.com>
2015-10-12 10:07:34 -0400

    document the base SQLA task on transactions
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/async/__init__.py
+++ chacra/async/__init__.py
@@ -30,8 +30,14 @@ models.init_model()
 
 
 class SQLATask(celery.Task):
-    """An abstract Celery Task that ensures that the connection the the
-    database is closed on task completion"""
+    """
+    An abstract Celery Task that ensures that the connection the the
+    database is closed on task completion
+
+    .. note:: On logs, it may appear as there are errors in the transaction but
+    this is not an error condition: SQLAlchemy rolls back the transaction if no
+    change was done.
+    """
     abstract = True
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
