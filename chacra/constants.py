3c76b0d7bbe554c5e86abcfcbd51162136c87e05
Andrew Schoen <aschoen@redhat.com>
2015-10-28 10:01:06 -0500

    add a constants.py module to hold the DISTRIBUTIONS list
    This list will be used to create project specific distributions files
    for debian-based repositories.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

---
+++ chacra/constants.py
@@ -0,0 +1,15 @@
+# a list of distro versions that chacra will generate a distributions file for
+DISTRIBUTIONS = [
+    'sid',
+    'stretch',
+    'jessie',
+    'wheezy',
+    'squeeze',
+    'precise',
+    'quantal',
+    'saucy',
+    'trusty',
+    'utopic',
+    'vivid',
+    'wily',
+]
