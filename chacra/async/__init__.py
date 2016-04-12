68915b7100acdae39118173c6d169eb6be1a0558
Alfredo Deza <adeza@redhat.com>
2016-04-12 12:36:58 -0400

    deb repo and rpm repo should use the build_repos queue
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/async/__init__.py
+++ chacra/async/__init__.py
@@ -62,11 +62,15 @@ def poll_repos():
             if r.type == 'rpm':
                 create_rpm_repo.apply_async(
                     (r.id,),
-                    countdown=pecan.conf.quiet_time)
+                    countdown=pecan.conf.quiet_time,
+                    queue='build_repos',
+                    )
             elif r.type == 'deb':
                 create_deb_repo.apply_async(
                     (r.id,),
-                    countdown=pecan.conf.quiet_time)
+                    countdown=pecan.conf.quiet_time,
+                    queue='build_repos',
+                    )
             else:
                 _type = r.infer_type()
                 if _type is None:
