e737d97d306283ca3b621ce11d5eec4a674b1653
Alfredo Deza <adeza@redhat.com>
2016-06-22 15:02:43 -0400

    create a metrics module to push to statsd
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

---
+++ chacra/metrics.py
@@ -0,0 +1,101 @@
+"""
+A wrapper around python-statsd to automatically configure outgoing metrics so
+that it can include, at the highest level::
+
+    secret.hostname.module
+
+Production Graphite instances might require a secret key which should be
+prefixed to outgoing metrics, if not configured, metrics would start off from
+the short hostname::
+
+    hostname.module
+
+Instantiating metrics work similarly to creating a logger instance::
+
+    from metrics import Counter
+
+    counter = Counter(__name__)
+
+    counter += 1
+
+It is allowed to alter the naming appending a suffix::
+
+
+    def my_oddly_named_function():
+        counter = Counter(__name__, suffix='expensive_function')
+        while True:
+            ...
+            counter +=1
+
+This would append 'package.module' with `expensive_function`, but
+just the suffix (the last part of the scheme). For a production host, this
+counter could look like::
+
+    secret.chacra1.chacra.async.expensive_function
+
+
+Although not encouraged, it is possible to fully override with a custom name::
+
+
+    counter = Counter('custom.path')
+
+Which would cause to override the module path (secret key and hostname would
+prevail)::
+
+    secret.chacra1.custom.path
+
+"""
+
+import socket
+import pecan
+import statsd
+
+
+def short_hostname(socket):
+    """
+    Obtains remote hostname of the socket and cuts off the domain part
+    of its FQDN.
+    """
+    return socket.gethostname().split('.', 1)[0]
+
+
+def get_prefix(conf=None, host=None):
+    host = host or short_hostname()
+    conf = conf or pecan.conf
+    secret = getattr(conf, 'graphite_secret', None)
+
+    if secret:
+        prefix = "%s.%s" % (secret, host)
+    else:
+        prefix = "%s" % host
+
+    return prefix
+
+
+def append_suffix(name, suffix):
+    """
+    Helper to append a suffix the end of the name with a custom one. Useful for
+    private functions or signatures that require distinct separation from the
+    module.
+    """
+    name_parts = name.split('.')
+    name_parts.append(suffix)
+    return '.'.join(name_parts)
+
+
+def Counter(name, suffix=None):
+    if suffix:
+        name = append_suffix(name, suffix)
+    return statsd.Counter("%s.%s" % (get_prefix(), name))
+
+
+def Gauge(name, suffix=None):
+    if suffix:
+        name = append_suffix(name, suffix)
+    return statsd.Gauge("%s.%s" % (get_prefix(), name))
+
+
+def Timer(name, suffix=None):
+    if suffix:
+        name = append_suffix(name, suffix)
+    return statsd.Timer("%s.%s" % (get_prefix(), name))
