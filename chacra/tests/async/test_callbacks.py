fe5887acf1dc42afd3bbd1af637738353acd6101
Alfredo Deza <adeza@redhat.com>
2016-08-10 12:22:21 -0400

    tests: add tests for callback and helpers
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/tests/async/test_callbacks.py
+++ chacra/tests/async/test_callbacks.py
@@ -1,7 +1,11 @@
+import os
 import pytest
 from pecan import conf
+import requests
 from chacra import async
+from chacra.async import recurring
 from chacra.models import Repo, Project
+from chacra.tests import conftest
 
 
 repo_keys = [
@@ -22,9 +26,59 @@ class TestHelpers(object):
             )
 
     @pytest.mark.parametrize('key', repo_keys)
-    def test_post_request(self, monkeypatch, recorder, key):
+    def test_post_request(self, session, recorder, key):
         conf.callback_url = 'http://localhost/callback'
         f_async = recorder()
         async.post_status('building', self.repo, _callback=f_async)
         result = f_async.recorder_calls[0]['kwargs']['args'][0]
         assert key in result
+        assert '"building"' in result
+
+
+class TestCallbackInvalidConf(object):
+
+    def setup(self):
+        conf.callback_url = 'http://localhost/callback'
+
+    def teardown(self):
+        conftest.reload_config()
+
+    def test_missing_user_and_key(self):
+        assert recurring.callback("{}", 'ceph') is False
+
+    def test_missing_user(self):
+        conf.callback_key = 'key'
+        assert recurring.callback("{}", 'ceph') is False
+
+    def test_missing_key(self):
+        conf.callback_user = 'admin'
+        assert recurring.callback("{}", 'ceph') is False
+
+
+class TestCallback(object):
+
+    def setup(self):
+        conf.callback_url = 'http://localhost/callback'
+        conf.callback_user = 'admin'
+        conf.callback_key = 'key'
+
+    def test_invalid_json(self):
+        # omg this is so invalid
+        assert recurring.callback({'error': Exception}, 'ceph') is False
+
+    def test_requests_correct_project_url(self, monkeypatch, recorder):
+        r = recorder()
+        monkeypatch.setattr(recurring.requests, 'post', r)
+        recurring.callback("{}", 'ceph')
+        result = r.recorder_calls[0]['args'][0]
+        assert result == os.path.join(conf.callback_url, 'ceph', '')
+
+    def test_requests_http_error(self, monkeypatch, fake):
+        # not confident this is testing what really happens. Could not get the
+        # 'retry' behavior from Celery. This just executes the code path to get
+        # to the exception being re-raised by Celery, which might be enough
+        def bad_post(*a, **kw):
+            raise requests.HTTPError('I suck')
+        monkeypatch.setattr(recurring.requests, 'post', bad_post)
+        with pytest.raises(requests.HTTPError):
+            recurring.callback("{}", 'ceph')
