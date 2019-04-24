957f80550ffbd91aa451c241e9342d63590ecba5
Alfredo Deza <adeza@redhat.com>
2019-04-24 15:44:48 -0400

    auth: decode to utf-8
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- chacra/auth.py
+++ chacra/auth.py
@@ -6,7 +6,7 @@ def basic_auth():
     try:
         auth = request.headers.get('Authorization')
         assert auth
-        decoded = base64.b64decode(auth.split(' ')[1])
+        decoded = base64.b64decode(auth.split(' ')[1]).decode('utf-8')
         username, password = decoded.split(':')
 
         assert username == conf.api_user
