313f1de1a21429c159a5e37304bf1ec2cf34db6e
Dan Mick <dan.mick@redhat.com>
2025-01-16 20:12:28 -0800

    requirements.txt, setup.py: unlock psycopg2 version
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         "pecan",
         "sqlalchemy==1.3.0",
-        "psycopg2==2.8.2",
+        "psycopg2",
         "pecan-notario",
         "python-statsd",
         "requests",
