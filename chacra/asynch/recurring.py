39fa0767b7ebd2b0a7d5db490812b322e2346955
Dan Mick <dan.mick@redhat.com>
2025-08-04 20:25:08 -0700

    various: add code to find and set UTC on py < 3.11
    because currently jammy has 3.10 and it's easy
    
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- chacra/asynch/recurring.py
+++ chacra/asynch/recurring.py
@@ -10,6 +10,11 @@ from celery import shared_task
 from chacra import models
 from chacra.asynch import base, debian, rpm, post_queued, post_deleted
 import logging
+try:
+    from datetime import UTC
+except:
+    import datetime
+    UTC=datetime.timezone.utc
 
 logger = logging.getLogger(__name__)
 
@@ -67,7 +72,7 @@ def purge_repos(_now=None):
         return
 
     # default value for repo life
-    now = _now or datetime.datetime.now(datetime.UTC)
+    now = _now or datetime.datetime.now(UTC)
     default_lifespan = now - datetime.timedelta(days=14)
     default_keep_minimum = 0
 
